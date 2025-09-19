e078bbf279335c3fbbcb21f2514e30b48f702af5
Joseph Mitchel <ujoe0346chi@gmail.com>
2025-09-19 05:54:40 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./src/**/*.{html,js}"],
+   theme: {
+     extend: {
+      colors: {
+        brand: {
+          hex1: ' #414652' ,
+          hex2: ' #92400e' ,
+          hex3: ' #f3f4f6',
+        },
+      }
+     },
+   },
+   
+   darkMode:'class',
+   plugins: [],
+ }
